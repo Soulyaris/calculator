5e4a48981d322b6fa1d9c0e8f059c68ce5e23356
Soulyaris <genessa174@gmail.com>
2019-06-04 12:47:53 +0500

    several improvements, added keyboard support

--- script/script.js
+++ script/script.js
@@ -7,7 +7,7 @@ const numeric = document.querySelectorAll('.numeric');
 const clrCalc = document.querySelector('#bclr');
 const bkspc = document.querySelector('#bdel');
 const dot = document.querySelector('#bdot');
-
+const body = document.body;
 let operand = 1;
 
 firstOperand.innerText = 0;
@@ -28,12 +28,12 @@ operators.forEach((oper) => {
 })
 
 let evaluate = (operandum, val) => 
-  (operandum.innerHTML == "0") ? val : (operandum.innerText.length < 20) ? operandum.innerHTML + val : operandum.innerHTML;
+  (operandum.innerText == "0" || operandum.innerText == "ERROR") ? val : (operandum.innerText.length < 20) ? operandum.innerText + val : operandum.innerText;
 
 numeric.forEach((num) => {
   num.addEventListener('click', (e) => {
-    (operand === 1) ? firstOperand.innerHTML = evaluate(firstOperand, e.srcElement.value) :
-      secOperand.innerHTML = evaluate(secOperand, e.srcElement.value);
+    (operand === 1) ? firstOperand.innerText = evaluate(firstOperand, e.srcElement.value) :
+      secOperand.innerText = evaluate(secOperand, e.srcElement.value);
   })
 })
 
@@ -65,4 +65,31 @@ result.addEventListener('click', (e) => {
   secOperand.innerText = "";
   operatorView.innerText = "";
   operand = 1;
+})
+
+body.addEventListener('keydown', (e) => {
+  console.log(e.key);
+  switch (e.key){
+    case "0": document.getElementById("b0").click(); break;
+    case "1": document.getElementById("b1").click(); break;
+    case "2": document.getElementById("b2").click(); break;
+    case "3": document.getElementById("b3").click(); break;
+    case "4": document.getElementById("b4").click(); break;
+    case "5": document.getElementById("b5").click(); break;
+    case "6": document.getElementById("b6").click(); break;
+    case "7": document.getElementById("b7").click(); break;
+    case "8": document.getElementById("b8").click(); break;
+    case "9": document.getElementById("b9").click(); break;
+    case "+": document.getElementById("badd").click(); break;
+    case "-": document.getElementById("bsub").click(); break;
+    case "/": document.getElementById("bdiv").click(); break;
+    case "*": document.getElementById("bmul").click(); break;
+    case "Backspace": document.getElementById("bdel").click(); break;
+    case "Enter":
+    case "=": document.getElementById("bres").click(); break;
+    case ".": document.getElementById("bdot").click(); break;
+    case "Delete":
+    case "c":
+    case "C": document.getElementById("bclr").click(); break;
+  };
 })
